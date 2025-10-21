39b528cc83d643d5a6d9b282ff45baab5178886a
ivan meyer <meyerivanoliver@gmail.com>
2025-10-21 19:25:19 -0300

    template inicial

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Guia PHP</title>
+  <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+  <h1>Guía de PHP - Iván Meyer</h1>
+  <ul>
+    <li><a href="ejercicios/ejercicio1.php">Ejercicio 1</a></li>
+  </ul>
+</body>
+</html>
+
